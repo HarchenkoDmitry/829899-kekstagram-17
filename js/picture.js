3f7d67f4c792e7628e9a6cfb2ac330f1fee419a9
Дмитрий <harchenkodmitrii@bk.ru>
2019-07-06 19:00:08 +0300

    refactoring picture

--- js/picture.js
+++ js/picture.js
@@ -2,25 +2,38 @@
 
 (function () {
   var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  var picturesContainer = document.querySelector('.pictures');
 
   function Picture(data) {
     this.url = data.url;
     this.likes = data.likes;
     this.description = data.description;
     this.comments = data.comments;
+
+    this.pictureClone = pictureTemplate.cloneNode(true);
+
+    this.pictureClone.addEventListener('click', render);
+
+    function render() {
+      window.renderFullScreenPhoto(data);
+    }
+
+    this.remove = function (pictureItem) {
+      this.pictureClone.removeEventListener('click', render);
+      picturesContainer.removeChild(pictureItem);
+    };
   }
 
   Picture.prototype.create = function () {
-    var pictureClone = pictureTemplate.cloneNode(true);
-    var img = pictureClone.querySelector('.picture__img');
-    var likes = pictureClone.querySelector('.picture__likes');
-    var comments = pictureClone.querySelector('.picture__comments');
+    var img = this.pictureClone.querySelector('.picture__img');
+    var likes = this.pictureClone.querySelector('.picture__likes');
+    var comments = this.pictureClone.querySelector('.picture__comments');
 
     img.src = this.url;
     likes.textContent = this.likes;
     comments.textContent = String(this.comments.length);
 
-    return pictureClone;
+    return this.pictureClone;
   };
 
 
