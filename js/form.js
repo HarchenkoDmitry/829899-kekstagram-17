ce6e0456d1d07c8d68c0931a8165409c7a4d4a88
Дмитрий <harchenkodmitrii@bk.ru>
2019-07-06 12:22:36 +0300

    added modal window and full screen photo

--- js/form.js
+++ js/form.js
@@ -2,38 +2,27 @@
 
 (function () {
   var form = document.querySelector('.img-upload__form');
-  var closeForm = form.querySelector('.img-upload__cancel');
-  var modalContainer = form.querySelector('.img-upload__overlay');
   var inputUploadFile = form.querySelector('#upload-file');
 
-  function showForm() {
-    modalContainer.classList.remove('hidden');
-    document.addEventListener('keydown', onFormEscPress);
-  }
+  var formModal = new window.Modal('.img-upload__overlay');
 
-  function onFormEscPress(evt) {
+  formModal.onModalEscPress = function (evt) {
     if (evt.keyCode === 27 && !document.activeElement.classList.contains('text__description')) {
-      hideForm();
+      formModal.close();
     }
-  }
+  };
 
-  function hideForm() {
-    modalContainer.classList.add('hidden');
-    document.removeEventListener('keydown', onFormEscPress);
+  formModal.onClose = function () {
     form.reset();
     window.resetPhotoChanges();
-  }
+  };
 
   inputUploadFile.addEventListener('change', function () {
     var file = inputUploadFile.files[0];
     if (~file.type.indexOf('image')) {
-      showForm();
+      formModal.open();
 
       window.applyEffectOnImage();
     }
-  });
-
-  closeForm.addEventListener('click', function () {
-    hideForm();
   });
 })();
