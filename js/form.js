f04f4c17b0e010c9cc72cb7d2e6aca15429bb83e
Дмитрий <harchenkodmitrii@bk.ru>
2019-07-07 23:57:11 +0300

    added validation hash tags

--- js/form.js
+++ js/form.js
@@ -3,12 +3,58 @@
 (function () {
   var form = document.querySelector('.img-upload__form');
   var inputUploadFile = form.querySelector('#upload-file');
+  var inputHashTags = form.querySelector('.text__hashtags');
+  var inputComment = form.querySelector('.text__description');
 
   var formModal = new window.Modal('.img-upload__overlay');
 
+  function addValidationHashTags() {
+    var arrayHashTags = inputHashTags.value
+      .split(' ')
+      .map(function (hashTag) {
+        return hashTag.toLowerCase();
+      });
+
+    var message = '';
+
+    if (arrayHashTags.length > 5) {
+      message = 'Нельзя указать больше пяти хэш-тегов';
+
+    } else {
+      for (var i = 0; i < arrayHashTags.length; i++) {
+
+        if (arrayHashTags[i].charAt(0) !== '#') {
+          message = 'Хеш-теги должны начинаться с \"#\"';
+          break;
+
+        } else if (arrayHashTags[i].length === 1) {
+          message = 'Хеш-теги должны состоять хотя бы из одного символа';
+          break;
+
+        } else if (arrayHashTags[i].indexOf('#', 1) !== -1) {
+          message = 'Хеш-теги должны разделяться пробелами';
+          break;
+
+        } else if (arrayHashTags.indexOf(arrayHashTags[i], i + 1) !== -1) {
+          message = 'Один и тот же хэш-тег не может быть использован дважды';
+          break;
+
+        } else if (arrayHashTags[i].length >= 20) {
+          message = 'Максимальная длина одного хэш-тега 20 символов';
+          break;
+        }
+      }
+    }
+
+    inputHashTags.setCustomValidity(message);
+  }
+
   formModal.onModalEscPress = function (evt) {
-    if (evt.keyCode === 27 && !document.activeElement.classList.contains('text__description')) {
-      formModal.close();
+    if (evt.keyCode === 27) {
+      var focusElement = document.activeElement;
+      if (focusElement !== inputComment && focusElement !== inputHashTags) {
+        formModal.close();
+      }
     }
   };
 
@@ -25,4 +71,9 @@
       window.applyEffectOnImage();
     }
   });
+
+  inputHashTags.addEventListener('input', function () {
+    addValidationHashTags();
+  });
+
 })();
