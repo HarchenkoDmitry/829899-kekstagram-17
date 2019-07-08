2e2063ae72961de00316b56b3fb7c2038707cb0a
Дмитрий <harchenkodmitrii@bk.ru>
2019-07-08 11:06:57 +0300

    fixed validation hash tags

--- js/form.js
+++ js/form.js
@@ -9,7 +9,7 @@
   var formModal = new window.Modal('.img-upload__overlay');
 
   function addValidationHashTags() {
-    var arrayHashTags = inputHashTags.value
+    var hashTags = inputHashTags.value
       .split(' ')
       .map(function (hashTag) {
         return hashTag.toLowerCase();
@@ -17,36 +17,38 @@
 
     var message = '';
 
-    if (arrayHashTags.length > 5) {
+    if (hashTags.length > 5) {
       message = 'Нельзя указать больше пяти хэш-тегов';
-
     } else {
-      for (var i = 0; i < arrayHashTags.length; i++) {
-
-        if (arrayHashTags[i].charAt(0) !== '#') {
-          message = 'Хеш-теги должны начинаться с \"#\"';
-          break;
-
-        } else if (arrayHashTags[i].length === 1) {
-          message = 'Хеш-теги должны состоять хотя бы из одного символа';
-          break;
-
-        } else if (arrayHashTags[i].indexOf('#', 1) !== -1) {
-          message = 'Хеш-теги должны разделяться пробелами';
-          break;
-
-        } else if (arrayHashTags.indexOf(arrayHashTags[i], i + 1) !== -1) {
-          message = 'Один и тот же хэш-тег не может быть использован дважды';
-          break;
-
-        } else if (arrayHashTags[i].length >= 20) {
-          message = 'Максимальная длина одного хэш-тега 20 символов';
+      for (var i = 0; i < hashTags.length; i++) {
+        message = validationHashTag(hashTags, i);
+        if (message) {
           break;
         }
       }
     }
 
     inputHashTags.setCustomValidity(message);
+  }
+
+  function validationHashTag(hashTags, i) {
+    var message = '';
+    if (hashTags[i].charAt(0) !== '#') {
+      message = 'Хеш-теги должны начинаться с "#"';
+
+    } else if (hashTags[i].length === 1) {
+      message = 'Хеш-теги должны состоять хотя бы из одного символа';
+
+    } else if (hashTags[i].indexOf('#', 1) > 0) {
+      message = 'Хеш-теги должны разделяться пробелами';
+
+    } else if (hashTags.indexOf(hashTags[i], i + 1) > 0) {
+      message = 'Один и тот же хэш-тег не может быть использован дважды';
+
+    } else if (hashTags[i].length > 20) {
+      message = 'Максимальная длина одного хэш-тега 20 символов';
+    }
+    return message;
   }
 
   formModal.onModalEscPress = function (evt) {
